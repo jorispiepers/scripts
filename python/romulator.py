7652050f35b7fca59c96217a766c1a5635e861f6
joris.piepers@gmail.com <joris.piepers@gmail.com>
2024-08-13 23:14:02 +0200

    UPdate romulator script

--- python/romulator.py
+++ python/romulator.py
@@ -1,82 +1,95 @@
-import os
-import re
+import zlib
+import os, sys
+import hashlib
+import platform
 import xml.etree.ElementTree as ET
 
 # Global vars
-romdir = './roms'
-datdir = "./dats"
-tosortdir = './tosort'
+romdir = 'roms'
+datdir = 'dats'
+tosortdir = 'tosort'
+
+# Detect platform
+osv = platform.system()
+
+# Clearing the Screen
+if 'Linux' in osv:
+    os.system('clear')
+elif 'Windows' in osv:
+    os.system('cls')
 
 # make sure subfolders still exist
 def make_dirs():
     try:
-        os.makedirs(tosortdir, mode=0o777, exist_ok=True)
-        os.makedirs(romdir, mode=0o777, exist_ok=True)
-        os.makedirs(datdir, mode=0o777, exist_ok=True)
+        osv = platform.system()
+        # We are using linux
+        if 'Linux' in osv:
+            os.makedirs(tosortdir, mode=0o777, exist_ok=True)
+            os.makedirs(romdir, mode=0o777, exist_ok=True)
+            os.makedirs(datdir, mode=0o777, exist_ok=True)
+        elif 'Windows' in osv:
+            if not os.path.exists(romdir):
+                os.mkdir(romdir)
+            if not os.path.exists(datdir):
+                os.mkdir(datdir)
+            if not os.path.exists(tosortdir):
+                os.mkdir(tosortdir)
     except:
         print("Couldn't create directories!")    
         exit(100)
 
-def search_namespace(line):
-    ns = re.search(r"xmlns:(\w+)=\"(.*)\"\s", line)
-    if ns is not None:
-        return ns
+def crc(fileName):
+    prev = 0
+    for eachLine in open(fileName,"rb"):
+        prev = zlib.crc32(eachLine, prev)
+    return "%X"%(prev & 0xFFFFFFFF)
+
+def md5(fileName):
+    m = hashlib.md5()
+    for eachLine in open(fileName, 'rb'):
+        m.update(eachLine)
+    return m.hexdigest()
 
 def open_dat_file():
     try:
         for root, subdirs, datfiles in os.walk(datdir):
-            # Probing namespace on dat files
             # TODO folder zip etc...
             for dat in datfiles:
-                with open(f'./{datdir}/{dat}', 'r') as input_file:
-                    xml = [ input_file.read() ]
-                    # Go through xml file and return namespace
-                    for element in xml:
-                        if 'datafile' in element:
-                            ns = search_namespace(element)
-                            namespace1 = ns[1]
-                            namespace2 = ns[2]
 
                 tree = ET.parse(f"./{datdir}/{dat}")
                 root = tree.getroot()
-                ET.register_namespace(namespace1, namespace2)
 
-                # create subfolders for rom sets
+                # Create subfolders for rom sets in case they do not exist
                 for romset in root.findall('.//header/name'):
-                    rset = f"./{romdir}/{romset.text}"
-                    os.makedirs(rset, mode=0o777, exist_ok=True)
-                
-                items = list(tree.iter())
-                for i, item in enumerate(items):
-                    print(i)
-                    print(item.tag)
-                    if item.tag == "game":
-                        item.values()
-#                        next_item = items[i + 1]
+                    if 'Linux' in osv:                    
+                        linux = f"./{romdir}/{romset.text}"
+                        os.makedirs(linux, mode=0o777, exist_ok=True)
+                    elif 'Windows' in osv:
+                        win = f".\{romdir}\{romset.text}"
+                        if not os.path.exists(win):
+                            os.mkdir(win)
 
-#                for game_name in root:
-                    #for game in game_name.items():
-                        # Adding name
-                        #if 'name' in game[0]:
-                        #    print("Game name: {}".format(game[1]))
-                        #if 'id' in game[0]:
-                        #    print("Game id: {}".format(game[1]))
+                # Printing name attributes
+                for game in root.iter('game'):
+                    for key, value in game.items():
+                        if 'name' in key:
+                            print("Game name: {}".format(value))
+                        if 'id' in key:
+                            print("Game id: {}".format(value))
 
-                        #print(game_name.attrib.values())
-#                        for rom_name in root.iter():
-###                            for rom in rom_name.items():
-  #                              if 'name' in rom[0]:
-  #                                  print(rom[0])
-  #                                  print("ROM name: {}".format(rom[1]))
-                                #if 'size' in rom[0]:
-                                #    print("ROM size: {}".format(rom[1]))
-                                #if 'crc' in rom[0]:
-                                #    print("ROM CRC: {}".format(rom[1]))
-                                #if 'md5' in rom[0]:
-                                #    print("ROM MD5: {}".format(rom[1]))
-                                #if 'sha1' in rom[0]:
-                                #    print("ROM SHA1: {}".format(rom[1]))
-
+                    # Printing rom attributes
+                    for rom in game.iter('rom'):
+                        for key, value in rom.items():
+                            if 'name' in key:
+                                print("ROM name: {}".format(value))
+                            if 'size' in key:
+                                print("ROM size: {}".format(value))
+                            if 'crc' in key:
+                                print("ROM CRC: {}".format(value))
+                            if 'md5' in key:
+                                print("ROM MD5: {}".format(value))
+                            if 'sha1' in key:
+                                print("ROM SHA1:{}".format(value))
 
     except:
         print("Couldn't read dat files ...")
