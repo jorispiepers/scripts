2a83e3fcfcb964ff9d8c39f8a09d5291cc4af7f1
joris.piepers@gmail.com <joris.piepers@gmail.com>
2024-08-15 08:05:25 +0200

    Updating romulator script

--- python/romulator.py
+++ python/romulator.py
@@ -1,13 +1,19 @@
+import os
+import re
 import zlib
-import os, sys
+#import py7zr
+import shutil
+import zipfile
 import hashlib
 import platform
 import xml.etree.ElementTree as ET
 
 # Global vars
-romdir = 'roms'
-datdir = 'dats'
-tosortdir = 'tosort'
+romDir = 'roms'
+datDir = 'dats'
+tosortDir = 'tosort'
+packedDir = 'tosort/packed'
+extractDir = 'tosort/unpacked'
 
 # Detect platform
 osv = platform.system()
@@ -21,22 +27,49 @@ elif 'Windows' in osv:
 # make sure subfolders still exist
 def make_dirs():
     try:
-        osv = platform.system()
-        # We are using linux
-        if 'Linux' in osv:
-            os.makedirs(tosortdir, mode=0o777, exist_ok=True)
-            os.makedirs(romdir, mode=0o777, exist_ok=True)
-            os.makedirs(datdir, mode=0o777, exist_ok=True)
-        elif 'Windows' in osv:
-            if not os.path.exists(romdir):
-                os.mkdir(romdir)
-            if not os.path.exists(datdir):
-                os.mkdir(datdir)
-            if not os.path.exists(tosortdir):
-                os.mkdir(tosortdir)
+        os.makedirs(romDir, mode=0o777, exist_ok=True)
+        os.makedirs(datDir, mode=0o777, exist_ok=True)
+        os.makedirs(tosortDir, mode=0o777, exist_ok=True)
+        os.makedirs(packedDir, mode=0o777, exist_ok=True)
+        os.makedirs(extractDir, mode=0o777, exist_ok=True)
     except:
         print("Couldn't create directories!")    
         exit(100)
+
+def unpack_files():
+    # Extracting packed files
+    for pathtoFile, subDirs, packedFile in os.walk(packedDir):
+        for name in packedFile:
+            if name.endswith('.zip'):
+                try:
+                    fileName = f'./{pathtoFile}/{name}'
+                    #with zipfile.ZipFile(fileName, 'r') as z:
+                    print("Extracting {} into {}".format(fileName, extractDir))
+                        #z.extractall(extractDir)
+                    regex = r'(.*)\/(.*)\/(.*)'
+                    splitff = re.search(regex, fileName)
+                    print("Move execution {} to {}".format(fileName, splitff[1]))
+                    #os.rename(fileName, splitff[1])
+                except Exception:
+                    pass
+    # Extracting remaining files and cleaning up extraction folder
+    for pathtoFile, subDirs, packedFile in os.walk(extractDir):
+        for name in packedFile:
+            if name.endswith('.zip'):
+                fileName = f'./{pathtoFile}/{name}'
+                try:
+                    #with zipfile.ZipFile(fileName, 'r') as z:
+                    print("Extracting {}".format(fileName))
+                    #z.extractall(extractDir)
+                    #print("Successfully extracted {} file.".format(fileName))
+                    print("Removing compressed file {}".format(fileName))
+                    os.remove(fileName)
+                except Exception:
+                    pass
+
+#def un7zip(fileName, extractDir):
+#    with py7zr.SevenZipFile(fileName, mode='r') as z:
+#        z.extractall(extractDir)
 
 def crc(fileName):
     prev = 0
@@ -50,26 +83,27 @@ def md5(fileName):
         m.update(eachLine)
     return m.hexdigest()
 
-def open_dat_file():
+def process_dat_file():
     try:
-        for root, subdirs, datfiles in os.walk(datdir):
+        for root, subdirs, datfiles in os.walk(datDir):
             # TODO folder zip etc...
             for dat in datfiles:
 
-                tree = ET.parse(f"./{datdir}/{dat}")
+                tree = ET.parse(f"./{datDir}/{dat}")
                 root = tree.getroot()
 
                 # Create subfolders for rom sets in case they do not exist
                 for romset in root.findall('.//header/name'):
                     if 'Linux' in osv:                    
-                        linux = f"./{romdir}/{romset.text}"
+                        linux = f"./{romDir}/{romset.text}"
                         os.makedirs(linux, mode=0o777, exist_ok=True)
                     elif 'Windows' in osv:
-                        win = f".\{romdir}\{romset.text}"
+                        win = f".\{romDir}\{romset.text}"
                         if not os.path.exists(win):
                             os.mkdir(win)
 
-                # Printing name attributes
+                # Getting general file name attribute
+                # for file renaming purposes
                 for game in root.iter('game'):
                     for key, value in game.items():
                         if 'name' in key:
@@ -77,24 +111,35 @@ def open_dat_file():
                         if 'id' in key:
                             print("Game id: {}".format(value))
 
-                    # Printing rom attributes
+                    # Getting actual file name attribute
                     for rom in game.iter('rom'):
                         for key, value in rom.items():
-                            if 'name' in key:
-                                print("ROM name: {}".format(value))
+                            # TODO size comparison
                             if 'size' in key:
                                 print("ROM size: {}".format(value))
+
+                            # TODO compare CRC
                             if 'crc' in key:
                                 print("ROM CRC: {}".format(value))
+
+                            # TODO compare MD5
                             if 'md5' in key:
                                 print("ROM MD5: {}".format(value))
+
+                            # TODO compare SHA1
                             if 'sha1' in key:
                                 print("ROM SHA1:{}".format(value))
+
+                            # After file checks are validated rename file
+                            # and put it in the appropriate folders
+                            if 'name' in key:
+                                print("ROM name: {}".format(value))
 
     except:
         print("Couldn't read dat files ...")
         exit(200)
 
 make_dirs()
-open_dat_file()
+unpack_files()
+process_dat_file()
 
